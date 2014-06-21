ee958b86e88d969c2f9aedd1147da84c7fa93b22
Alicia Perez <pericia@gmail.com>
2014-06-21 14:37:58 -0400

    Second attempt to colour found string in red when searching for events

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -34,67 +34,92 @@ $(document).ready(function() {
 
 
 
-});
+	});
 
 
-		if($("#event_has_length").is(':checked')){
-			$("#hidden_to_field").show();
-			$("#time_label").html("From");
-		}		
-		else{
-			$("#hidden_to_field").hide();
-			$("#time_label").html("At");
-		}
+	if($("#event_has_length").is(':checked')){
+		$("#hidden_to_field").show();
+		$("#time_label").html("From");
+	}		
+	else{
+		$("#hidden_to_field").hide();
+		$("#time_label").html("At");
+	}
 
 
 
 	$(document).on('click', ".clickable_row", function () {
         //window.document.location = $(this).data('url');
         $(location).attr('href', $(this).data('url'));
-    });
+      });
 
 
 	$(".clickable_row").hover(
-	  function() {
-	    $(this).addClass("row_hovered");
+		function() {
+			$(this).addClass("row_hovered");
 
-	  }, function() {
-	    $(this).removeClass("row_hovered");
-	  }
+		}, function() {
+			$(this).removeClass("row_hovered");
+		}
 	);
 
 	$("#popup").on('click', function () {
-//use http://jqueryui.com/dialog/#modal-form
-//or angular: http://codepen.io/ionic/pen/zkmhJ?editors=101
-// or this: http://jsfiddle.net/cUfYw/
-    });
- 
+		//use http://jqueryui.com/dialog/#modal-form
+		//or angular: http://codepen.io/ionic/pen/zkmhJ?editors=101
+		// or this: http://jsfiddle.net/cUfYw/
+	});
+
 
 
 
 //------------add red color to search term----------------------
-var title = $("span[class='search']").html();
-//alert(title);
+function replaceAll(find, replace, str) {
+	return str.replace(new RegExp(find, 'g'), replace);
+}
+
+/**
+function funct(){
+	var title = $("span[class='search']").html();
+	console.log(title);
+
+	var search_term = $("h2[class='search']").data("search-term");
+	console.log("search_term="+search_term);
+
+	if(search_term != undefined){
+		var vari = replaceAll(search_term, "<span>"+search_term+"</span>", title);
+		//console.log(vari);
+		$("span[class='search_title']").html(vari);
+
+		$("span[class='search_title'] span").addClass("red");
+		//console.log($("span[class='search_title']").html());
+	}
+}
+*/
+
+
+function red(){
+	//console.log("entra");
+	var title = $(this).html();
+	//console.log(title);
+
+	if(search_term != undefined){
+		//console.log("search_term="+search_term);
+		var vari = replaceAll(search_term, "<span>"+search_term+"</span>", title);
+		console.log("vari="+vari);
+		$(this).html(vari);
+
+		$(this).children('span').addClass("red");
+		//console.log($("span[class='search_title']").html());
+	}
+}
+
+
+
+
 var search_term = $("h2[class='search']").data("search-term");
-//alert(search_term);
-var index = title.indexOf(search_term);
-var found = title.substr(index, search_term.length); //start, length
-var piece = "<span class='red'>"+found+"</span>";
-//alert(piece);
-
-
-//FROM <span class="search">Movie</span>
-// if I search for 'ov' it should change
-//TO   M<span>ov</span>ie
-
-//convert html:
-$("span[class='search_title']").html(title.substr(0, index)+piece+title.substr(index+search_term.length, title.length));
+//console.log("search_term="+search_term);
+$("span[class='search_title'] span").each( red );
 //------------end of add red color to search term----------------------
-
-
-
-
-
 
 
 }); //document.ready
